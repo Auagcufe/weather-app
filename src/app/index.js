9f2e2350f1241f2e154d24d6c0766cbb0d1c75d3
Auagcufe <bwang092@gmail.com>
2019-08-01 15:13:57 +0800

    Starting template

--- src/app/index.js
+++ src/app/index.js
@@ -1,2 +1,14 @@
 import React from 'react'
+import HomeScreen from './screens/HomeScreen'
 
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <HomeScreen />
+            </div>
+        )
+    }
+}
+
+export default App
---
+++ src/app/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class HomeScreen extends React.Component{
+    render(){
+        return(
+            <div>Hello Home!!!</div>
+        )
+    }
+}
+
+
+
+export default HomeScreen
